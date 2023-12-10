fa250919e9f5be94b3daa7a4f1bdaff2568dc03d
ghimiremukesh <ghimiremukesh1@gmail.com>
2023-12-10 00:43:34 -0700

    updates

--- script10.js
+++ script10.js
@@ -1,6 +1,6 @@
 window.onload = choosePic;
 
-var myPix = new Array("Photos/acadia.jpg", "Photos/golden_gate.jpg", "Photos/mounthood.jpg", "Photos/multonomah.jpg", "Photos/rainier.jpg", "Photos/yosemite.jpg")
+var myPix = new Array("Photos/acadia.jpg", "Photos/golden_gate.jpg", "Photos/mounthood.jpg", "Photos/multonomah.jpg", "Photos/rainier.jpg", "Photos/yosemite.jpg", "Photos/london_bridge.HEIC", "Photos/stonehenge.HEIC", "Photos/oxford.HEIC")
 
 function choosePic() {
     var randomNum = Math.floor(Math.random() * myPix.length);
